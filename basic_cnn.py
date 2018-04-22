38fd40d93b3f67ae1d51f43107a157b683782b6f
lburger98 <lburger98@gmail.com>
2018-04-22 17:02:22 -0400

    almost working

--- basic_cnn.py
+++ basic_cnn.py
@@ -3,24 +3,32 @@
 from fruit_loader import fruit_loader
 import tensorflow as tf
 
+print("loading data")
+test_images, test_labels = fruit_loader('test')
+train_images, train_labels = fruit_loader('train')
 
 #######################################
 #####  Load in data  ##################
 #######################################
 
-fruit, labels = fruit_loader()
-fruit_ds = tf.data.Dataset.from_tensor_slices(fruit)
-print(fruit_ds)
-exit()
-
-
 #######################################
 #####  Build neural net  ##############
 #######################################
 
+
+def next_batch(array, batch_size, index):
+    length = len(array)
+    if (index*batch_size)+batch_size >= length:
+        index = (length/batch_size) % index
+    batch = array[index*batch_size:(index*batch_size)+batch_size]
+    return batch
+
+
+print("initalizing network")
+
 sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+x = tf.placeholder(tf.float32, shape=[None, 45, 45, 3])
+y_ = tf.placeholder(tf.float32, shape=[None, 60])
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -36,12 +44,9 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 1, 32])
+W_conv1 = weight_variable([5, 5, 3, 32])
 b_conv1 = bias_variable([32])
-
-x_image = tf.reshape(x, [-1,28,28,1])
-
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 W_conv2 = weight_variable([5, 5, 32, 64])
@@ -59,32 +64,33 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+W_fc2 = weight_variable([1024, 60])
+b_fc2 = bias_variable([60])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
 #train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-train_step = tf.train.AdamOptimizer(learning_rate=.0001, beta1=0.9, beta2=0.999).minimize(cross_entropy)
-'''
-learning_rate=.001 >>>>>> 0.9929
-learning_rate=.0001 >>>>>> 0.9932
-learning_rate=.0005 >>>>>> 0.9927
-learning_rate=.0002 >>>>>> 0.9917
-learning_rate=.00009 >>>>>> 0.9925
+train_step = tf.train.AdamOptimizer(learning_rate=.0001).minimize(cross_entropy)
 
-keep_prob: 1.0 >>>>>> 0.9823
-'''
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
-for i in range(20000):
-	batch = mnist.train.next_batch(50)
-	if i%100 == 0:
-		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 0.95})
-	print("step %d, training accuracy %g"%(i, train_accuracy))
-	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+batch_size = 50
+num_epochs = 5
+
+print ("starting train")
+for epoch in range(num_epochs):
+    for i in range(len(train_images) // batch_size):
+    	batch = (next_batch(train_images, 50, i), next_batch(train_labels, 50, i))
+    	if i%100 == 0:
+    		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 0.95})
+    		print("step %d, training accuracy %g"%(i, train_accuracy))
+    	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    print("Epoch: %g"%epoch)
+    print("test accuracy %g"%accuracy.eval(feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0}))
 
 sess.close()
