bda16d6abb46f1752ed095aafa3aa83aec5efa1d
Collin Bentley <collin@Collins-MBP.wireless.yale.internal>
2018-04-22 20:39:07 -0400

    got rid of cv2

--- fruit_loader.py
+++ fruit_loader.py
@@ -1,8 +1,9 @@
 import numpy as np
 import tensorflow as tf
 import glob
-import cv2
+# import cv2
 import os
+from PIL import Image
 
 def fruit_loader(data_type='train'):
     fruit_images = []
@@ -14,15 +15,23 @@ def fruit_loader(data_type='train'):
     for fruit_dir_path in glob.glob(path):
         fruit_label = fruit_dir_path.split("/")[-1]
         for image_path in glob.glob(os.path.join(fruit_dir_path, "*.jpg")):
-            image = cv2.imread(image_path,cv2.IMREAD_COLOR)
-            image = cv2.resize(image, (45, 45))
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            fruit_images.append(image)
+#           image = cv2.imread(image_path,cv2.IMREAD_COLOR)
+#           image = cv2.resize(image, (45, 45))
+#           image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            img = Image.open(image_path)
+            wpercent = (28/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((28,hsize))
+            fruit_images.append(img)
             labels.append(fruit_label)
-    fruit_images = np.array(fruit_images)
+            print(image_path)
+            print("successful")
+    print(fruit_images)
+    fruit_images = np.concatenate(fruit_images, axis=0)
     fruit_images = fruit_images/float(255)
-    # fruit_images = fruit_images.reshape(fruit_images.shape[0], 45*45*3)
+    #fruit_images = fruit_images.reshape(fruit_images.shape[0], 100*100)
 
     labels = np.array(labels)
     label_to_id_dict = {v:i for i,v in enumerate(np.unique(labels))}
