b086e3cd1d365a532fd8127d88f9fe09b53bb3cf
lburger98 <lburger98@gmail.com>
2018-04-23 22:34:58 -0400

    using RGB and added red circle (accuracy 94%)

--- basic_cnn.py
+++ basic_cnn.py
@@ -2,10 +2,21 @@
 
 from fruit_loader import fruit_loader
 import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+# Uncomment to use CPU instead of GPU
+# os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
 
 print("loading data")
 test_images, test_labels = fruit_loader('test')
 train_images, train_labels = fruit_loader('train')
+
+# plt.imshow(test_images[0], interpolation='nearest')
+# plt.show()
+# exit()
 
 #######################################
 #####  Load in data  ##################
@@ -17,6 +28,7 @@ train_images, train_labels = fruit_loader('train')
 
 
 def next_batch(array, batch_size, index):
+    index = index + 1
     length = len(array)
     if (index*batch_size)+batch_size >= length:
         index = (length/batch_size) % index
@@ -26,71 +38,64 @@ def next_batch(array, batch_size, index):
 
 print("initalizing network")
 
-sess = tf.InteractiveSession()
+number_of_fruits = 10
+
 x = tf.placeholder(tf.float32, shape=[None, 45, 45, 3])
-y_ = tf.placeholder(tf.float32, shape=[None, 60])
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+labels = tf.placeholder(tf.float32, shape=[None, number_of_fruits])
 
 
-W_conv1 = weight_variable([5, 5, 3, 32])
-b_conv1 = bias_variable([32])
-h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+conv1 = tf.layers.conv2d(
+      inputs=x,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
+conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+pool2_flat = tf.reshape(pool2, [-1, 11 * 11 * 64])
 
-
+# Dense Layer
 keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+dropout = tf.layers.dropout(inputs=dense, rate=(1-keep_prob))
 
-W_fc2 = weight_variable([1024, 60])
-b_fc2 = bias_variable([60])
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+# Logits Layer
+logits = tf.layers.dense(inputs=dropout, units=number_of_fruits)
 
+# Loss and optimizer
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-#train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-train_step = tf.train.AdamOptimizer(learning_rate=.0001).minimize(cross_entropy)
-
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+# Calculate accuracy
+correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.global_variables_initializer())
 
 
 batch_size = 50
-num_epochs = 5
+num_epochs = 100
+init = tf.global_variables_initializer()
 
-print ("starting train")
-for epoch in range(num_epochs):
-    for i in range(len(train_images) // batch_size):
-    	batch = (next_batch(train_images, 50, i), next_batch(train_labels, 50, i))
-    	if i%100 == 0:
-    		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 0.95})
-    		print("step %d, training accuracy %g"%(i, train_accuracy))
-    	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print("Epoch: %g"%epoch)
-    print("test accuracy %g"%accuracy.eval(feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0}))
+with tf.Session() as sess:
+  init.run()
 
-sess.close()
+  print ("starting train")
+  for epoch in range(num_epochs):
+      for i in range(len(train_images) // batch_size):
+      	batch = (next_batch(train_images, 50, i), next_batch(train_labels, 50, i))
+      	train_step.run(feed_dict={x: batch[0], labels: batch[1], keep_prob: 0.5})
+
+      print("Epoch: %g"%epoch)
+      print("train accuracy %g"%accuracy.eval(feed_dict={x:batch[0], labels: batch[1], keep_prob: 0.95}))
+      print("test accuracy %g"%accuracy.eval(feed_dict={x: test_images, labels: test_labels, keep_prob: 1.0}))
+
+  sess.close()
--- fruit_loader.py
+++ fruit_loader.py
@@ -1,34 +1,68 @@
 import numpy as np
 import tensorflow as tf
 import glob
-import cv2
+# import cv2
 import os
+from PIL import Image, ImageDraw
+
+number_of_fruits = 10
+
 
 def fruit_loader(data_type='train'):
-    fruit_images = []
-    labels = []
-    path = './fruits-360/Training/*'
-    if data_type == 'test':
-        path = './fruits-360/Validation/*'
+	fruit_images = []
+	labels = []
+	path = './fruits-360/Training/*'
+	if data_type == 'test':
+		path = './fruits-360/Validation/*'
 
-    for fruit_dir_path in glob.glob(path):
-        fruit_label = fruit_dir_path.split("/")[-1]
-        for image_path in glob.glob(os.path.join(fruit_dir_path, "*.jpg")):
-            image = cv2.imread(image_path,cv2.IMREAD_COLOR)
-            image = cv2.resize(image, (45, 45))
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+	counter = 0
+	for fruit_dir_path in glob.glob(path):
+		fruit_label = fruit_dir_path.split("/")[-1]
 
-            fruit_images.append(image)
-            labels.append(fruit_label)
-    fruit_images = np.array(fruit_images)
-    fruit_images = fruit_images/float(255)
-    # fruit_images = fruit_images.reshape(fruit_images.shape[0], 45*45*3)
+		counter += 1
+		if counter > number_of_fruits:
+			break
+		print(fruit_label)
 
-    labels = np.array(labels)
-    label_to_id_dict = {v:i for i,v in enumerate(np.unique(labels))}
-    id_to_label_dict = {v: k for k, v in label_to_id_dict.items()}
-    label_ids = np.array([label_to_id_dict[x] for x in labels])
-    one_hot_labels = tf.one_hot(label_ids, len(np.unique(label_ids)))
-    one_hot_labels = tf.Session().run(one_hot_labels)
+		for image_path in glob.glob(os.path.join(fruit_dir_path, "*.jpg")):
 
-    return (fruit_images, one_hot_labels)
+			# image = cv2.imread(image_path,cv2.IMREAD_COLOR)
+			# image = cv2.resize(image, (45, 45))
+			# image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+			img = Image.open(image_path)
+
+			# draw red circle in center
+			x, y =  img.size
+			eX, eY = 20, 20 #Size of Bounding Box for ellipse
+			bbox =  (x/2 - eX/2, y/2 - eY/2, x/2 + eX/2, y/2 + eY/2)
+			draw = ImageDraw.Draw(img)
+			draw.ellipse(bbox, fill=(255, 0, 0))
+			del draw
+
+			wpercent = (45/float(img.size[0]))
+			hsize = int((float(img.size[1])*float(wpercent)))
+			img = img.resize((45,hsize))
+
+			img_array = np.array(img)
+			# img_array = img_array[:, :, 0:1]
+
+			#fruit_images = np.concatenate(img_array, axis=0)
+			fruit_images.append(img_array)
+			labels.append(fruit_label)
+
+			img.close()
+
+	fruit_images = np.stack(fruit_images, axis=0)
+	fruit_images = fruit_images/float(255)
+	#fruit_images = fruit_images.reshape(fruit_images.shape[0], 100*100)
+
+	labels = np.array(labels)
+
+	label_to_id_dict = {v:i for i,v in enumerate(np.unique(labels))}
+	id_to_label_dict = {v: k for k, v in label_to_id_dict.items()}
+	label_ids = np.array([label_to_id_dict[x] for x in labels])
+	one_hot_labels = tf.one_hot(label_ids, len(np.unique(label_ids)))
+	one_hot_labels = tf.Session().run(one_hot_labels)
+
+	return (fruit_images, one_hot_labels)
